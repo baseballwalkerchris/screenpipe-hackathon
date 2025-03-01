0c0a66a0f402d577f7fd02fae2b66bc9daf28749
DanielWong76 <danielwong0505@gmail.com>
2025-03-01 15:33:25 -0500

    Started creating projects screen

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+import Sidebar from "@/components/ui/sidebar";
+
+export default function ProjectsPage() {
+  return (
+    <div className="flex h-screen bg-secondary-background">
+      {/* Sidebar Navigation */}
+      <Sidebar />
+    </div>
+  )
+}
