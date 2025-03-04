84ed5c80ab19bca925c7d057aecd56125837ef3d
DanielWong76 <danielwong0505@gmail.com>
2025-03-03 19:30:30 -0500

    Routed Landing page to projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,5 @@
-"use client";
+import { redirect } from 'next/navigation';
 
-import { SettingsProvider } from "@/lib/settings-provider";
-import { ClientOnly } from "@/lib/client-only";
-import { Inter } from "next/font/google";
-import { UserTest } from "@/components/user-test";
-
-const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-inter",
-});
-
-export default function Page() {
-  return (
-    <SettingsProvider>
-      <ClientOnly>
-        <div className={`h-full ${inter.className}`}>
-          <UserTest onDataChange={() => {}} />
-        </div>
-      </ClientOnly>
-    </SettingsProvider>
-  );
+export default function Home() {
+  redirect('/projects');
 }
